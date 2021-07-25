4af5c50de0a3f715e2cc8ff74108d40d48686c25
Erik Godoy <erik.sgodoy@gmail.com>
2021-07-24 21:28:43 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/erikgodoy/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.erikgodoy.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.erikgodoy.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
